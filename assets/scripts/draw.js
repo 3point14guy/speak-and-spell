95d77841bef51b63237cac3052c56fcec0a34769
3poitn14guy <nicholas.piaskoski@gmail.com>
2018-10-21 12:22:03 -0400

    add draw screen
    add draw.html .css and .js
    add links btwn html pages

---
+++ assets/scripts/draw.js
@@ -0,0 +1,58 @@
+const canvas = document.querySelector('#draw');
+// ctx is abbr. for context
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 50;
+// dummy variable
+let isDrawing = false
+let lastX = 0;
+let lastY = 0;
+let hue = 0
+let direction = true;
+
+function draw(e) {
+  // stops the function from running when they are not moused down
+  if(!isDrawing) return;
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  //*** these:
+  // lastX = e.offsetX;
+  // lastY = e.offsetY;
+  // are the same as this denatured array:
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue ++
+    if(hue >= 360) {
+      hue = 0;
+    }
+  if(ctx.lineWidth >= 75 || ctx.lineWidth <= 5) {
+    direction = !direction;
+  }
+  if(direction) {
+    ctx.lineWidth ++;
+  } else {
+    ctx.lineWidth --;
+  }
+
+
+}
+
+canvas.addEventListener('mousedown', (e) => {
+isDrawing = true;
+[lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener('mousemove', draw);
+
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
